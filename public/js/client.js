2624c0544630877769bf5c3210d34235f56c8efa
Venkatesan <v.venkatesan@healthgrades.com>
2020-12-16 11:47:10 +0530

    remove unneccesary console.log calls

--- public/js/client.js
+++ public/js/client.js
@@ -1,5 +1,3 @@
-console.log("javascript file");
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
